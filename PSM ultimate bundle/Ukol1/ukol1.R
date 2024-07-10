91ed44ce83876e06d4bd7a5d0dc68a673b6494b9
Pepa Bůh <128801641+PalacinkaVelika@users.noreply.github.com>
2024-07-10 16:14:20 +0200

    Ukol 1 DONE

--- PSM ultimate bundle/Ukol1/ukol1.R
+++ PSM ultimate bundle/Ukol1/ukol1.R
@@ -1,5 +1,7 @@
 library(MASS)
 library(DescTools)
+library(effsize)
+
 
 ## vysledky jazykovych testu zavisi na socioekonomickem statusu ?
 # U socioekonomickeho statusu porovnavejte pouze 5 skupin vytvorenych podle pocatecni cislice promenne SES.
@@ -11,22 +13,44 @@ jazykovy_test <- nlschools$lang
 socioeko_status <- nlschools$SES
 # vykreslení prvních deseti hodnot ať vím s čím pracuji
 head(jazykovy_test, 10)
-head(socioeko_status, 10)
+head(socioeko_status, 20)
 # socioeko na kategorie
-kategorie <- factor(socioeko_status, levels = levels(kategorie)[1:5])
-# Kategorie jsou 10, 13, 15, 17, 18
-# normalita?
+prvni_cislice <- as.integer(substr(socioeko_status, 1, 1))
+kategorie <- factor(prvni_cislice, levels = 1:5)
+
+
+# Chci porovnat statisticky významný rozdíl skupin = ANOVA nebo něco podobného
+# Potřebuju zjistit - normalita reziduí, shodnost rozptylů
+
+# normalita reziduí?
 res<-residuals(lm(jazykovy_test~kategorie))
 PlotQQ(res) # podle QQ je šikmý doleva
-shapiro.test(res) # test souhlasí že není normální rozdělení (p = 0.02)
+shapiro.test(res) # test souhlasí že není v žádném případě normální rozdělení (p = 1.294e-15)
+# Normalita = NE => Kruskal
+
 boxplot(jazykovy_test~kategorie,main="vysledky testu podle socioeko statusu",col="orange")
-# test variability
-oneway.test(jazykovy_test~kategorie, var.equal = FALSE)
-# rozptyly jsou různé (p = 0.0007558) => Existuje minimálně jedna dvojice se statisticky výzmannou rozdílností
-TukeyHSD(aov(jazykovy_test~factor(kategorie), var.equal = FALSE))
-plot(TukeyHSD(aov(jazykovy_test~factor(kategorie))))
-# věcná významnost
-# věcně významné jsou rozdíly mezi skupinami 10 - 15, 10 - 18
+
+kruskal.test(jazykovy_test ~ kategorie) # průměry se liší
+# Které skupiny se liší?
+DunnTest(jazykovy_test~kategorie)
+# Liší se všechny skupiny
+
+# Kruskal nepotřebuje test rozptylu, ale protože je žádán tak ho sem dám
+bartlett.test(jazykovy_test~kategorie)
+#  p-value = 1.03e-05 => rozptyl nejsou stejné
+
+# Věcná významnost
+# Budu kontrolovat všechny páry cohenovým D
+cohen.d(jazykovy_test ~ factor(kategorie, levels = c(levels(kategorie)[1], levels(kategorie)[2]))) # mid
+cohen.d(jazykovy_test ~ factor(kategorie, levels = c(levels(kategorie)[1], levels(kategorie)[3]))) # mid
+cohen.d(jazykovy_test ~ factor(kategorie, levels = c(levels(kategorie)[1], levels(kategorie)[4]))) # large
+cohen.d(jazykovy_test ~ factor(kategorie, levels = c(levels(kategorie)[1], levels(kategorie)[5]))) # large
+cohen.d(jazykovy_test ~ factor(kategorie, levels = c(levels(kategorie)[2], levels(kategorie)[3]))) # small
+cohen.d(jazykovy_test ~ factor(kategorie, levels = c(levels(kategorie)[2], levels(kategorie)[4]))) # mid
+cohen.d(jazykovy_test ~ factor(kategorie, levels = c(levels(kategorie)[2], levels(kategorie)[5]))) # mid
+cohen.d(jazykovy_test ~ factor(kategorie, levels = c(levels(kategorie)[3], levels(kategorie)[4]))) # small
+cohen.d(jazykovy_test ~ factor(kategorie, levels = c(levels(kategorie)[3], levels(kategorie)[5]))) # mid
+cohen.d(jazykovy_test ~ factor(kategorie, levels = c(levels(kategorie)[4], levels(kategorie)[5]))) # small
 
 
 
@@ -49,6 +73,22 @@ plot(hc, hang = -1)
 k <- 5
 plot(hc)
 rect.hclust(hc, k)
+
+
+
+
+cereal_data$cluster <- cutree(hc, k)
+cluster_means <- aggregate(. ~ cluster, data = cereal_data, FUN = mean)
+print(cluster_means)
+# Skupina 1 = 
+# Skupina 2 = 
+# Skupina 3 = nejvíc kalorií, proteinu, tuku, fibre, carbo, sugars and potassium
+# Skupina 4 = nejvíc sodium
+# Skupina 5 = 
+
+
+
+
 # Protože jde o dataset kde má vliv hodně proměnných najednou mi nedává smysl použít k-means nebo jiný dvoudimensionální
 # přístup.
 
@@ -78,3 +118,21 @@ fact_anal
 #Tento faktor má vysokou váhu pro cukry (sugars).
 #Ukazuje, že cukry jsou samostatnou složkou, která může být v cereáliích přítomna nezávisle na ostatních nutričních hodnotách.
 
+
+
+par(mfrow = c(1, 3))  # Rozmístění grafů do jednoho řádku a tří sloupců
+
+# Graf Faktor 1 vs. Faktor 2
+plot(fact_anal$scores[,1], fact_anal$scores[,2], 
+     xlab = "Faktor 1 (Nutriční hodnoty)", ylab = "Faktor 2 (Energetické složky)",
+     main = "Faktorové skóry cereálií - F1 vs. F2")
+
+# Graf Faktor 1 vs. Faktor 3
+plot(fact_anal$scores[,1], fact_anal$scores[,3], 
+     xlab = "Faktor 1 (Nutriční hodnoty)", ylab = "Faktor 3 (Složení uhlohydrátů)",
+     main = "Faktorové skóry cereálií - F1 vs. F3")
+
+# Graf Faktor 2 vs. Faktor 3
+plot(fact_anal$scores[,2], fact_anal$scores[,3], 
+     xlab = "Faktor 2 (Energetické složky)", ylab = "Faktor 3 (Složení uhlohydrátů)",
+     main = "Faktorové skóry cereálií - F2 vs. F3")
